abe26ba8f136efa9b2062845f9cc477573ad5067
VTI\truyen.nguyentuan <truyen.nguyentuan@vti.com.vn>
2024-06-25 14:46:27 +0700

    impl kafka

---
+++ src/main/java/vn/com/dsk/demo/features/kafka/KafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package vn.com.dsk.demo.features.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsApplication.class, args);
+    }
+
+}
